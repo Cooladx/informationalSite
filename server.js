3e0e9b5b7162747b5a321f749832d30913ab6c3f
Mike Nguyen <111297303+cooladx@users.noreply.github.com>
2025-11-08 01:24:32 -0500

    added comments on how fs works on reading html file.

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-#!/usr/bin/node
+
+// pulling http to create server
 const { createServer } = require('node:http');
+
+// file system to be able to read/write to files
 const fs = require('node:fs');
 
-const { URL } = require('url'); 
+
 
 // hostname and port to link page
 const hostname = '127.0.0.1';
@@ -11,25 +14,31 @@ const port = 8080;
 // Create server
 const server = createServer((req, res) => {
   
+    // Gets the URL after the hostname and ensures status code 200 on user going to initital url
     const url = req.url
     res.statusCode = 200;
+    // Set the header to html so user can see initial page
     res.setHeader('Content-Type', 'text/html');
   
+    // Root URL
     if (url === "/") {
     readSite('index.html', res)
     }
 
+    // URL for about page
     else if (url === "/about") {
     readSite('about.html', res)
     }
 
+    // URL for contact page
      else if (url === "/contact") {
     readSite('contact.html', res);
     }
 
+
+    // In the case user types incorrect URL, pop up the error page and send a 404.
     else {
       res.statusCode = 404;
-      res.setHeader('Content-Type', 'text/html');
       readSite('404.html', res);
     }
     
@@ -44,20 +53,16 @@ server.listen(port, hostname, () => {
 
 
 
-// Reading files of other htmls files to serve
+// Function to read file of an html file to serve
 const readSite = (filename, res) => {
-
-fs.readFile(filename, 'utf8', (err, data) => {
+  // Read in a file of html and return error. Otherwise, return the data and end the link.
+  fs.readFile(filename, 'utf8', (err, data) => {
     if (err) {
       console.error(err);
       return;
     }
-    else {
-    console.log(data);
-    }
     res.end(data)
   });
-  
 }
 
 
